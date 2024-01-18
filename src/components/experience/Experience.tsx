b8da0b8eb96c6050db9efa1898ed15af32e611f9
davidho <david.ho@chaintechdev.com>
2024-01-18 15:02:25 +0800

    feat: add dynamic fov for responsiveness

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -19,18 +19,19 @@ import {
 
 import '@/components/experience/Experience.css'
 
-const MutableCameraPositions = [new THREE.Vector3(0.046, 0.7857, 1.9249), new THREE.Vector3(0.21, 0.2357, 0.24)]
-const MutableCameraLookAts = [new THREE.Vector3(0, 0.3, 0), new THREE.Vector3(0.39, 0.17, 0)]
-
 interface OceanShaderMaterialUniform {
     uBigWavesElevation: number
     uSmallWavesElevation: number
 }
-
 type OceanRefType = React.MutableRefObject<OceanShaderMaterialUniform>
+
+const MutableCameraPositions = [new THREE.Vector3(0.046, 0.7857, 1.9249), new THREE.Vector3(0.21, 0.2357, 0.24)]
+const MutableCameraLookAts = [new THREE.Vector3(0, 0.3, 0), new THREE.Vector3(0.39, 0.17, 0)]
 
 function Scene() {
     const scene = useThree((state) => state.scene)
+    const camera = useThree((state) => state.camera)
+
     const scrollData = useScroll()
     const cameraControlRef = useRef() as React.RefObject<CameraControls>
     const cameraPositionCurveRef = useRef<THREE.CatmullRomCurve3>(new THREE.CatmullRomCurve3(MutableCameraPositions))
@@ -55,9 +56,7 @@ function Scene() {
         const isInFirstSection = scrollData.visible(0, 3 / scrollPages)
         let nextCameraPosition
         let nextCameraLookAt
-
         // cameraControlRef.current.setLookAt(position[0], position[1], position[2], lookAt[0], lookAt[1], lookAt[2], true)
-
         if (cameraControlRef.current) {
             cameraControlRef.current.disconnect()
             cameraControlRef.current.smoothTime = 0.3
@@ -69,7 +68,6 @@ function Scene() {
                 nextCameraPosition = cameraPositionCurveRef.current.getPoint(firstSectionOffset)
                 nextCameraLookAt = cameraLookAtCurveRef.current.getPoint(firstSectionOffset)
             }
-
             const cameraDistance = cameraControlRef.current.camera.position.distanceTo(nextCameraLookAt)
             const clampedDistanceFactor = getClampedValue(Math.pow(cameraDistance, 3), 0, 1.5)
 
@@ -85,71 +83,77 @@ function Scene() {
         }
     })
 
-    // useEffect(() => {
-    //     // Initial adjustment for responsive camera path
-    //     let isMobileLegacyData = false
-    //     const perfectWindowWidth = 1920
-    //     const canvas = scene.getObjectByName('canvas')
-    //     const lighthouseGroup = scene.getObjectByName('lighthouseScene')
-    //     const canvasGlobalPosition = new THREE.Vector3()
-    //     const resizeMobileScene = () => {
-    //         lighthouseGroup?.scale.set(0.7, 0.7, 0.7)
-    //         if (oceanRef.current) {
-    //             ;(oceanRef as OceanRefType).current.uBigWavesElevation = 0.1
-    //             ;(oceanRef as OceanRefType).current.uSmallWavesElevation = 0.12
-    //         }
+    useEffect(() => {
+        // Initial adjustment for responsive camera path
+        // let isMobileLegacyData = false
+        // const canvas = scene.getObjectByName('canvas')
+        // const lighthouseGroup = scene.getObjectByName('lighthouseScene')
+        // const canvasGlobalPosition = new THREE.Vector3()
+        // const resizeMobileScene = () => {
+        //     lighthouseGroup?.scale.set(0.7, 0.7, 0.7)
+        //     if (oceanRef.current) {
+        //         ;(oceanRef as OceanRefType).current.uBigWavesElevation = 0.1
+        //         ;(oceanRef as OceanRefType).current.uSmallWavesElevation = 0.12
+        //     }
 
-    //         canvas?.getWorldPosition(canvasGlobalPosition)
-    //         MutableCameraPositions[1].set(0.058, -0.033, 0)
-    //         MutableCameraLookAts[1] = canvasGlobalPosition
-    //         isMobileLegacyData = true
-    //     }
+        //     canvas?.getWorldPosition(canvasGlobalPosition)
+        //     MutableCameraPositions[1].set(0.058, -0.033, 0)
+        //     MutableCameraLookAts[1] = canvasGlobalPosition
+        //     isMobileLegacyData = true
+        // }
 
-    //     const resetScene = () => {
-    //         MutableCameraPositions[1].set(
-    //             defaultCameraPositions[1][0],
-    //             defaultCameraPositions[1][1],
-    //             defaultCameraPositions[1][2]
-    //         )
+        // const resetScene = () => {
+        //     MutableCameraPositions[1].set(
+        //         defaultCameraPositions[1][0],
+        //         defaultCameraPositions[1][1],
+        //         defaultCameraPositions[1][2]
+        //     )
 
-    //         lighthouseGroup?.scale.set(1, 1, 1)
+        //     lighthouseGroup?.scale.set(1, 1, 1)
 
-    //         if (oceanRef.current) {
-    //             ;(oceanRef as OceanRefType).current.uBigWavesElevation = 0.15
-    //             ;(oceanRef as OceanRefType).current.uSmallWavesElevation = 0.15
-    //         }
+        //     if (oceanRef.current) {
+        //         ;(oceanRef as OceanRefType).current.uBigWavesElevation = 0.15
+        //         ;(oceanRef as OceanRefType).current.uSmallWavesElevation = 0.15
+        //     }
 
-    //         MutableCameraLookAts[1].setY(defaultCameraLookAts[1][1])
-    //         MutableCameraLookAts[1].setZ(defaultCameraLookAts[1][2])
-    //         isMobileLegacyData = false
-    //     }
+        //     MutableCameraLookAts[1].setY(defaultCameraLookAts[1][1])
+        //     MutableCameraLookAts[1].setZ(defaultCameraLookAts[1][2])
+        //     isMobileLegacyData = false
+        // }
 
-    //     const handleExperienceResize = () => {
-    //         const isMobile = window.innerWidth <= 768
-    //         const responsiveCameraOffset = (perfectWindowWidth - window.innerWidth) * 0.00001
+        const perfectWindowWidth = 1920
+        const handleResizeExperience = () => {
+            if (window.innerWidth < perfectWindowWidth) {
+                (camera as THREE.PerspectiveCamera).fov = (perfectWindowWidth - window.innerWidth) / 40 + 60
+            } else {
+                (camera as THREE.PerspectiveCamera).fov = 60
+            }
+            camera.updateProjectionMatrix()
+            // const isMobile = window.innerWidth <= 768
+            // const responsiveCameraOffset = (perfectWindowWidth - window.innerWidth) * 0.00001
 
-    //         if (isMobile) {
-    //             resizeMobileScene()
-    //         } else {
-    //             if (isMobileLegacyData) {
-    //                 resetScene()
-    //             }
+            // if (isMobile) {
+            //     resizeMobileScene()
+            // } else {
+            //     if (isMobileLegacyData) {
+            //         resetScene()
+            //     }
 
-    //             MutableCameraPositions[1].setX(defaultCameraPositions[1][0] - responsiveCameraOffset)
-    //             MutableCameraLookAts[1].setX(defaultCameraLookAts[1][0] - responsiveCameraOffset)
-    //         }
+            //     MutableCameraPositions[1].setX(defaultCameraPositions[1][0] - responsiveCameraOffset)
+            //     MutableCameraLookAts[1].setX(defaultCameraLookAts[1][0] - responsiveCameraOffset)
+            // }
 
-    //         cameraPositionCurveRef.current = new THREE.CatmullRomCurve3(MutableCameraPositions)
-    //         cameraLookAtCurveRef.current = new THREE.CatmullRomCurve3(MutableCameraLookAts)
-    //     }
+            // cameraPositionCurveRef.current = new THREE.CatmullRomCurve3(MutableCameraPositions)
+            // cameraLookAtCurveRef.current = new THREE.CatmullRomCurve3(MutableCameraLookAts)
+        }
 
-    //     handleExperienceResize()
-    //     addEventListener('resize', handleExperienceResize)
+        handleResizeExperience()
+        addEventListener('resize', handleResizeExperience)
 
-    //     return () => {
-    //         removeEventListener('resize', handleExperienceResize)
-    //     }
-    // }, [])
+        return () => {
+            removeEventListener('resize', handleResizeExperience)
+        }
+    }, [])
 
     return (
         <>
