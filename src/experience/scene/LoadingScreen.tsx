5dc94156ce22e5cff9fc13a2106c127e3676dba6
davidho <david.ho@chaintechdev.com>
2024-04-03 17:41:43 +0800

    fix: fix progress flickering issue

--- src/experience/scene/LoadingScreen.tsx
+++ src/experience/scene/LoadingScreen.tsx
@@ -4,22 +4,28 @@ import { useProgress } from '@react-three/drei'
 import LighthouseLoadingSvg from '@/assets/svgs/lighthouse_loading.svg'
 
 export default function LoadingScreen() {
-    const progress = useProgress((state) => state.progress)
-    const [isLoaded, setIsLoaded] = useState(false)
+    const totalItems = 14
+    const loadedItems = useProgress((state) => state.loaded)
+    const isLoaded = loadedItems === totalItems
+
+    const [isStarted, setIsStarted] = useState(false)
 
     return (
         <section
             className='fixed flex h-screen w-screen flex-col items-center justify-center bg-primary transition-opacity duration-500 ease-linear'
-            style={{ opacity: isLoaded ? 0 : 1, pointerEvents: isLoaded ? 'none' : 'all' }}
+            style={{ opacity: isStarted ? 0 : 1, pointerEvents: isStarted ? 'none' : 'all' }}
         >
-            <object type='image/svg+xml' data={LighthouseLoadingSvg} className='w-[400px]'>
-                <img src={LighthouseLoadingSvg} />
-            </object>
+            <figure className='h-[200px]'>
+                <object type='image/svg+xml' data={LighthouseLoadingSvg} className='w-[400px]'>
+                    <img src={LighthouseLoadingSvg} />
+                </object>
+            </figure>
+
             <div className='mt-4 text-lg font-extrabold text-secondary'>
-                {progress === 100 ? (
-                    <button onClick={() => setIsLoaded(true)}>Start</button>
+                {isLoaded ? (
+                    <button onClick={() => setIsStarted(true)}>Start</button>
                 ) : (
-                    <h1>Loading {progress}%</h1>
+                    <h1>Loading {Math.round((loadedItems / totalItems) * 100)}%</h1>
                 )}
             </div>
         </section>
