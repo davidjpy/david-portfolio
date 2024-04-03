dd9f699489f493a855a104e920331826b34fd014
davidho <david.ho@chaintechdev.com>
2024-04-03 17:41:06 +0800

    fix: remove unused suspense

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <Suspense fallback={null}>
-            <App />
-        </Suspense>
+        <App />
     </React.StrictMode>
 )
