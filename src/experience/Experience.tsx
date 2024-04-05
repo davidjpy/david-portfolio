cc79812596eacb16540aaabaef2a8bb713f0f52a
davidho <david.ho@chaintechdev.com>
2024-04-05 17:41:09 +0800

    fix: remove unused pages

--- src/experience/Experience.tsx
+++ src/experience/Experience.tsx
@@ -9,18 +9,17 @@ import BrightnessSlider from '@/src/experience/htmls/BrightnessSlider'
 import Camera from '@/src/experience/camera/Camera'
 import { getClampedValue } from '@/src/utilities/getClampedValue'
 
-import { cameraConfig, scrollPages, perfectPageHeight } from '@/src/utilities/constants'
+import { cameraConfig } from '@/src/utilities/constants'
 
 import '@/experience/Experience.css'
 
 function Scene() {
     const oceanRef = useRef<unknown>(null)
     const camera = useThree((state) => state.camera) as THREE.PerspectiveCamera
-    const { isStarted } = useContext(AppContext)
 
     const perfectWindowWidth = 1920
     const [isMobile, setIsMobile] = useState(false)
-    const [pages, setPages] = useState(scrollPages * (perfectPageHeight / window.innerHeight))
+
 
     useEffect(() => {
         const handleResizeExperience = () => {
@@ -31,7 +30,6 @@ function Scene() {
                 setIsMobile(false)
                 camera.fov = getClampedValue((perfectWindowWidth - window.innerWidth) / 40 + 60, 60, 70)
             }
-            setPages(scrollPages * (perfectPageHeight / window.innerHeight))
             camera.updateProjectionMatrix()
         }
 
@@ -44,7 +42,7 @@ function Scene() {
     }, [])
 
     return (
-        <ScrollControls pages={1} damping={0}>
+        <ScrollControls damping={0}>
             <BrightnessSlider />
             <HtmlContent />
             <Suspense fallback={null}>
