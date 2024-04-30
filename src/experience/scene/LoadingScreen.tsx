5d1402506896ebb7ff9efe2b443881ca267528d1
David <david1999.hch@gmail.com>
2024-05-01 02:37:38 +0800

    feat: update loading page animation

--- src/experience/scene/LoadingScreen.tsx
+++ src/experience/scene/LoadingScreen.tsx
@@ -1,24 +1,31 @@
-import { useContext, useRef } from 'react'
+import { useContext, useEffect, useLayoutEffect, useRef } from 'react'
 import Lottie from 'lottie-react'
 
 import { AppContext } from '@/src/context/appContext'
-import animation from '@/assets/svgs/lighthouse.json'
+import lighthouseAnimation from '@/assets/svgs/lighthouse.json'
+import boatWheelAnimation from '@/assets/svgs/boat_wheel.json'
 
 import type { LottieRefCurrentProps } from 'lottie-react'
 
 export default function LoadingScreen() {
     const { isLoading, isStarted, setIsStarted } = useContext(AppContext)
-    const lottieRef = useRef<LottieRefCurrentProps | null>(null)
+    const lighthouseLottieRef = useRef<LottieRefCurrentProps | null>(null)
+    const boatWheelLottieRef = useRef<LottieRefCurrentProps | null>(null)
 
     const handleLoopAnimation = () => {
-        if (lottieRef.current) {
-            lottieRef.current.goToAndPlay(4600, false)
+        if (lighthouseLottieRef.current) {
+            lighthouseLottieRef.current.playSegments([460, 900], true)
         }
     }
 
     const handleClickStartExperience = () => {
-        lottieRef.current?.destroy()
-        setIsStarted(true)
+        lighthouseLottieRef.current?.playSegments([900, 2000], true)
+
+        setTimeout(() => {
+            lighthouseLottieRef.current?.pause()
+            lighthouseLottieRef.current?.destroy()
+            setIsStarted(true)
+        }, 200)
     }
 
     return (
@@ -27,25 +34,40 @@ export default function LoadingScreen() {
             style={{ opacity: isStarted ? 0 : 1, pointerEvents: isStarted ? 'none' : 'all' }}
         >
             {isLoading ? (
-                <h1 className='text-4xl font-extrabold text-secondary'>
+                <h1 className='text-4xl font-extrabold text-[#505050]'>
                     Sailing To the <span className='text-accent'>Lighthouse</span>...
                 </h1>
             ) : (
                 <div className='text-center'>
                     <span>
                         <Lottie
-                            lottieRef={lottieRef}
-                            animationData={animation}
+                            lottieRef={lighthouseLottieRef}
+                            animationData={lighthouseAnimation}
+                            initialSegment={[0, 900]}
+                            autoPlay={false}
                             loop={false}
                             onComplete={handleLoopAnimation}
                             className='w-[600px]'
                         />
                     </span>
-                    {!isStarted && (
-                        <button onClick={handleClickStartExperience} className='mt-8'>
-                            Start
-                        </button>
-                    )}
+                    <button
+                        onClick={handleClickStartExperience}
+                        className='m-auto flex items-center gap-[8px] rounded-full border-[1px] border-[#505050] pb-[8px] pl-[20px] pr-[20px] pt-[8px] font-medium text-[#505050]'
+                    >
+                        Start Journey
+                        {!isStarted && (
+                            <span>
+                                <Lottie
+                                    lottieRef={boatWheelLottieRef}
+                                    animationData={boatWheelAnimation}
+                                    autoPlay={false}
+                                    loop={false}
+                                    initialSegment={[0, 100]}
+                                    className='h-[40px]'
+                                />
+                            </span>
+                        )}
+                    </button>
                 </div>
             )}
         </section>
