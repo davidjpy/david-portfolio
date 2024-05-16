be7cd8c5c4b4e262624dd85a56ac3489cdaf37b9
David <david1999.hch@gmail.com>
2024-05-16 21:41:29 +0800

    feat: add focus triggered transition

--- src/experience/htmls/PhotoMasonry.tsx
+++ src/experience/htmls/PhotoMasonry.tsx
@@ -1,8 +1,4 @@
-import { useEffect, useRef } from 'react'
-
-interface Props {
-    willChangePropObserverRef: React.MutableRefObject<IntersectionObserver | null>
-}
+import { useRef } from 'react'
 
 interface AnimatedPhotoProps {
     src: string
@@ -14,10 +10,15 @@ interface AnimatedPhotoProps {
 function AnimatedPhoto({ src, alt, title, description }: AnimatedPhotoProps) {
     return (
         <>
-            <img alt={alt} loading='lazy' src={src} className='life-list-img group-hover:scale-105' />
-            <span className='life-list-img-mask-bg group-hover:opacity-100'></span>
-            <span className='life-list-img-mask-frame group-hover:h-[75%] group-hover:w-[75%] group-hover:opacity-100 group-hover:backdrop-blur-[2px]' />
-            <figcaption className='life-list-img-caption group-hover:-translate-y-1/2 group-hover:opacity-100'>
+            <img
+                alt={alt}
+                loading='lazy'
+                src={src}
+                className='life-list-img group-hover:scale-105 group-focus:scale-105'
+            />
+            <span className='life-list-img-mask-bg group-hover:opacity-100 group-focus:opacity-100'></span>
+            <span className='life-list-img-mask-frame group-hover:h-[75%] group-hover:w-[75%] group-hover:opacity-100 group-hover:backdrop-blur-[2px] group-focus:h-[75%] group-focus:w-[75%] group-focus:opacity-100' />
+            <figcaption className='life-list-img-caption group-hover:-translate-y-1/2 group-hover:opacity-100 group-focus:-translate-y-1/2 group-focus:opacity-100'>
                 <h1 className='font-medium'>{title}</h1>
                 <p className='text-sm'>{description}</p>
             </figcaption>
@@ -25,20 +26,8 @@ function AnimatedPhoto({ src, alt, title, description }: AnimatedPhotoProps) {
     )
 }
 
-export default function PhotoMasonry({ willChangePropObserverRef }: Props) {
+export default function PhotoMasonry() {
     const photoGalleryRef = useRef<HTMLDivElement>(null)
-
-    // useEffect(() => {
-    //     if (photoGalleryRef.current) {
-    //         willChangePropObserverRef.current?.observe(photoGalleryRef.current)
-    //     }
-
-    //     return () => {
-    //         if (photoGalleryRef.current) {
-    //             willChangePropObserverRef.current?.unobserve(photoGalleryRef.current)
-    //         }
-    //     }
-    // }, [photoGalleryRef.current])
 
     return (
         <div
@@ -46,7 +35,7 @@ export default function PhotoMasonry({ willChangePropObserverRef }: Props) {
             className='mt-6 grid h-[900px] w-full grid-cols-3 grid-rows-12 gap-[12px] rounded-[4px] bg-primary-monochrome p-[12px] max-2xl:h-[1100px] max-2xl:grid-cols-2 max-mobile:h-[900px] max-mobile:grid-cols-3'
             ref={photoGalleryRef}
         >
-            <figure className='life-list-img-wrapper group col-span-1 row-span-6'>
+            <figure tabIndex={0} className='life-list-img-wrapper group col-span-1 row-span-6'>
                 <AnimatedPhoto
                     src='images/life/painting.webp'
                     alt='David is painting a golden retriever on canvas'
@@ -54,7 +43,7 @@ export default function PhotoMasonry({ willChangePropObserverRef }: Props) {
                     description='Art Workshop'
                 />
             </figure>
-            <figure className='life-list-img-wrapper group col-span-1 row-span-3'>
+            <figure tabIndex={0} className='life-list-img-wrapper group col-span-1 row-span-3'>
                 <AnimatedPhoto
                     src='images/life/hk_island.webp'
                     alt='Overlooking the South China Sea from Po Toi Island'
@@ -62,7 +51,7 @@ export default function PhotoMasonry({ willChangePropObserverRef }: Props) {
                     description='Po Toi Island'
                 />
             </figure>
-            <figure className='life-list-img-wrapper group col-span-1 row-span-5'>
+            <figure tabIndex={0} className='life-list-img-wrapper group col-span-1 row-span-5'>
                 <AnimatedPhoto
                     src='images/life/fire_dragon_dance.webp'
                     alt='A splashy red lightbox depicting the Fire Dragon Dance'
@@ -70,7 +59,7 @@ export default function PhotoMasonry({ willChangePropObserverRef }: Props) {
                     description='Fire Dragon Dance'
                 />
             </figure>
-            <figure className='life-list-img-wrapper group row-span-6'>
+            <figure tabIndex={0} className='life-list-img-wrapper group row-span-6'>
                 <AnimatedPhoto
                     src='images/life/thailand_dragon_tower.webp'
                     alt='A high tower with a Chinese dragon twining around it'
@@ -78,7 +67,7 @@ export default function PhotoMasonry({ willChangePropObserverRef }: Props) {
                     description='Dragon Tower'
                 />
             </figure>
-            <figure className='life-list-img-wrapper group col-span-1 row-span-7'>
+            <figure tabIndex={0} className='life-list-img-wrapper group col-span-1 row-span-7'>
                 <AnimatedPhoto
                     src='images/life/kumamoto_forest.webp'
                     alt='A stone road leading into a forest'
@@ -86,7 +75,7 @@ export default function PhotoMasonry({ willChangePropObserverRef }: Props) {
                     description='Forest'
                 />
             </figure>
-            <figure className='life-list-img-wrapper group col-span-1 row-span-6'>
+            <figure tabIndex={0} className='life-list-img-wrapper group col-span-1 row-span-6'>
                 <AnimatedPhoto
                     src='images/life/taiwan_graffiti.webp'
                     alt='A massive graffiti artwork depicting a robot on a tall building'
@@ -94,7 +83,7 @@ export default function PhotoMasonry({ willChangePropObserverRef }: Props) {
                     description='Graffiti'
                 />
             </figure>
-            <figure className='life-list-img-wrapper group col-span-1 row-span-3'>
+            <figure tabIndex={0} className='life-list-img-wrapper group col-span-1 row-span-3'>
                 <AnimatedPhoto
                     src='images/life/japan_house.webp'
                     alt='A block of traditional japanese architecture'
